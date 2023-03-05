cd27d7cd9642c245c5f4320de7616c5143e670ac
Surya-06 <126967680+Surya-06@users.noreply.github.com>
2023-03-05 14:08:03 +0530

    Add files via upload

---
+++ IsPrime.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		int prime=1;
+		boolean isPrime = true;
+		for (int i = 2; i <prime;) {
+			if(prime/i==0);
+			{
+				isPrime= false;
+				break;
+				
+			}
+			
+			
+		}
+		
+		if(isPrime) {
+			System.out.println(prime  + " is a Prime number ");
+		}
+		else
+		{
+			System.out.println("Not a prime number");
+		}
+		
+	}
+	} 
---
+++ Scooter.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class Scooter {
+
+	public static void main(String[] args) {
+		
+		int Wheels = 2;
+		char logo = 'H';
+		boolean isOld = false;
+		String ScooterName = "Honda Dio";
+		float  mileage = 45.5f;
+		long  numberplate = 12345678901l;
+		double  kms = 1000.4556;
+		
+		System.out.println("No of wheels in scooter : " + Wheels);
+		System.out.println("BrandLogo : " +  logo);
+		System.out.println("Scooter New or old : " + isOld);
+		System.out.println("Scooter numberplate : "  + numberplate);
+		System.out.println("Scooter name : " + ScooterName);
+		System.out.println("Scooter Mileage : " + mileage);
+		System.out.println("Scooter kilometer : " + kms);
+		
+		System.out.println("Vechile Details :  " + Wheels +  logo + isOld + numberplate + ScooterName  +  mileage +  kms  );
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
